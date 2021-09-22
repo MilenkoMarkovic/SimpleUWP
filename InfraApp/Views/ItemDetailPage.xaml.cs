83677d9f297b36f0a5612e25df66d7c92e788c71
MilenkoMarkovic <milenko.markovic@akvelon.com>
2021-09-22 11:18:44 +0200

    Commit alll SImple UWP files

---
+++ InfraApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using InfraApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace InfraApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
